cb81ceeaf2eac48c4d2c73f0e7e1c0fefecb400d
wglacier <w_glacier@hotmail.com>
2020-04-01 20:28:23 +1100

    update

--- gen_readme.py
+++ gen_readme.py
@@ -1,6 +1,7 @@
-import os
+import os,re
 
 outfile = 'README.md'
+outhtml_name = 'problems.htm'
 sol_path = './solutions/'
 
 header = """
@@ -12,22 +13,58 @@ header = """
 
 ---
 
-| Problems | Solutions |
-|----------|-----------|
+| No. | Problems |    Level  | Solutions |
+|-----|----------|-----------|-----------|
 """
+
+headerHtml = """
+<html>
+<title> LeetCode Solutions by wglacier </title>
+<body>
+<table>
+    <th>No.</th>
+    <th>Problems</th>
+    <th>Solutions</th>
+    <th>LeetCode Link</th>
+"""
+outhtml = open(outhtml_name, 'w')
 
 with open(outfile, 'w') as outf:
     outf.write(header)
+    outhtml.write(headerHtml)
     files = sorted(os.listdir(sol_path))
+    no = 0
     for fn in files:
         if not fn.endswith('.md'):
             continue
+        no += 1
         fbase = os.path.splitext(fn)[0]
         fname = fbase[6:] # without leading number
-        line = '| [{base}](https://leetcode.com/problems/{link}/) | [source](./solutions/{fn}) |'.format(
+        fnamelink = fname.replace('(', '').replace(')', '').replace(' ', '-').lower()
+        fn_name = fn.replace(' ', '%20')
+        locallink = './solutions/' + fn_name
+        leetlink = 'https://leetcode.com/problems/' + fnamelink
+        first_line = open(sol_path + fn, 'r').readline()
+        m = re.match(r'^[^,]+,\s*(\w+)',first_line)
+        level = m.group(1) if m else ''
+        line = '| {no} | <a href="{link}" target="_blank">{base}</a> | {level} | <a href="{fn}" target="_blank">source</a> |'.format(
+            no=no,
             base=fbase,
-            link=fname.replace('(', '').replace(')', '').replace(' ', '-').lower(),
-            fn=fn.replace(' ', '%20')
+            link=leetlink,
+            level=level,
+            fn=locallink
         )
+        
+
         outf.write(line + '\n')
+
+        linehtml = """<tr><td>%d</td>
+                      <td>%s</td>
+                      <td><a href="%s" target="_blank">Solutions</a></td>
+                      <td><a href="%s" target="_blank">LeetCode</a></td></tr>\n""" % (
+            no, fbase, locallink, leetlink)
+        outhtml.write(linehtml)
+
+outhtml.write("</table></body></html>")
+outhtml.close()
         
